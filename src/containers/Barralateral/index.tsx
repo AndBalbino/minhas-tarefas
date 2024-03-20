223828c832449e8c911280da41873cb4fac38873
Anderson Balbino <andersonbalbino2212@outlook.com>
2024-03-19 22:36:24 -0300

    Construindo a seção de tarefas e estilizando

--- src/containers/Barralateral/index.tsx
+++ src/containers/Barralateral/index.tsx
@@ -7,12 +7,12 @@ const BarraLateral = () => (
     <div>
       <S.Campo type="text" placeholder="Buscar" />
       <S.Filtros>
-        <FiltroCard />
-        <FiltroCard />
-        <FiltroCard />
-        <FiltroCard />
-        <FiltroCard />
-        <FiltroCard ativo />
+        <FiltroCard legenda="pendente" contador={1} />
+        <FiltroCard legenda="concluidas" contador={3} />
+        <FiltroCard legenda="urgentes" contador={5} />
+        <FiltroCard legenda="importantes" contador={3} />
+        <FiltroCard legenda="normal" contador={1} />
+        <FiltroCard legenda="todas" contador={13} ativo />
       </S.Filtros>
     </div>
   </S.Aside>
