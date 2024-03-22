801b51ffd212e881bec857c78b88e3f316aa6901
Anderson Balbino <andersonbalbino2212@outlook.com>
2024-03-21 22:13:52 -0300

    Avançando no projeto
    Trabalhando com props e  estizando as tags de tarefa conforme seus valores.

--- src/components/Tarefa/index.tsx
+++ src/components/Tarefa/index.tsx
@@ -1,16 +1,39 @@
+import { useState } from 'react'
 import * as S from './styles'
 
-const Tarefa = () => (
-  <S.Card>
-    <S.Titulo>Nome da tarefa</S.Titulo>
-    <S.Tag>Importante</S.Tag>
-    <S.Tag>Pendente</S.Tag>
-    <S.Descricao />
-    <S.BarraAcoes>
-      <S.Botao>Editar</S.Botao>
-      <S.Botao>Remover</S.Botao>
-    </S.BarraAcoes>
-  </S.Card>
-)
+type Props = {
+  titulo: string
+  prioridade: string
+  status: string
+  descricao: string
+}
+
+const Tarefa = ({ prioridade, descricao, status, titulo }: Props) => {
+  const [estaEditando, setEstaEditanto] = useState(false)
+
+  return (
+    <S.Card>
+      <S.Titulo>{titulo}</S.Titulo>
+      <S.Tag prioridade={prioridade}>{prioridade}</S.Tag>
+      <S.Tag status={status}>{status}</S.Tag>
+      <S.Descricao value={descricao} />
+      <S.BarraAcoes>
+        {estaEditando ? (
+          <>
+            <S.BotaoSalvar>Salvar</S.BotaoSalvar>
+            <S.BotaoCancelar onClick={() => setEstaEditanto(false)}>
+              Cancelar
+            </S.BotaoCancelar>
+          </>
+        ) : (
+          <>
+            <S.Botao onClick={() => setEstaEditanto(true)}>Editar</S.Botao>
+            <S.BotaoCancelar>Remover</S.BotaoCancelar>
+          </>
+        )}
+      </S.BarraAcoes>
+    </S.Card>
+  )
+}
 
 export default Tarefa
--- src/containers/ListaDeTarefas/index.tsx
+++ src/containers/ListaDeTarefas/index.tsx
@@ -1,30 +1,43 @@
 import Tarefa from '../../components/Tarefa'
 import { Container } from './styles'
 
+const tarefas = [
+  {
+    titulo: 'Estudar',
+    descricao: 'Ver video aula',
+    prioridade: 'importante',
+    status: 'pendente'
+  },
+  {
+    titulo: 'Ler',
+    descricao: 'ler o livro',
+    prioridade: 'urgente',
+    status: 'concluída'
+  },
+  {
+    titulo: 'Treinar',
+    descricao: 'Treinar perna',
+    prioridade: 'importante',
+    status: 'concluída'
+  }
+]
+
 const ListaDeTarefas = () => (
   <Container>
     <p>
       2 tarefas marcadas como &quot;categoridas&ldquo; e &quot;termo&ldquo;{' '}
     </p>
     <ul>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
+      {tarefas.map((t) => (
+        <li key={t.titulo}>
+          <Tarefa
+            descricao={t.descricao}
+            prioridade={t.prioridade}
+            status={t.status}
+            titulo={t.titulo}
+          />
+        </li>
+      ))}
     </ul>
   </Container>
 )
