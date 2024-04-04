c279af459b4b9b6f8f79f39a0be6fd148d2e45fe
Anderson Balbino <andersonbalbino2212@outlook.com>
2024-04-03 21:44:44 -0300

    Utilizando o React-Router

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,11 @@
+import BarraLateral from '../../containers/Barralateral'
+import ListaDeTarefas from '../../containers/ListaDeTarefas'
+
+const Home = () => (
+  <>
+    <BarraLateral />
+    <ListaDeTarefas />
+  </>
+)
+
+export default Home
